29ffa38c7a7c6f820595feefddeec544dd61b303
Candra <candraramadhanp@outlook.com>
2019-07-23 15:35:46 +0700

    First Commit

---
+++ CandraBrowser/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CandraBrowser
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                myWeb.GoBack();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                myWeb.GoForward();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                myWeb.Source = new Uri("http://" + seacrhText.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            string homepageUrl = Properties.Settings.Default.homepageUrl;
+
+            if (homepageUrl == null){
+                textKosong.Visibility = Visibility.Visible;
+            }else
+                try
+                {
+                    myWeb.Source = new Uri(homepageUrl);
+                    textKosong.Visibility = Visibility.Hidden;
+
+                }
+                catch
+                {
+
+                }
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            SettingWindows setWindow = new SettingWindows();
+            setWindow.ShowDialog();
+        }
+    }
+}
---
+++ CandraBrowser/SettingWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CandraBrowser
+{
+    /// <summary>
+    /// Interaction logic for SettingWindows.xaml
+    /// </summary>
+    public partial class SettingWindows : Window
+    {
+        public SettingWindows()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonSave_Click(object sender, RoutedEventArgs e)
+        {
+            Properties.Settings.Default.homepageUrl = textSetting.Text;
+            Properties.Settings.Default.Save();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            textSetting.Text = Properties.Settings.Default.homepageUrl;
+        }
+    }
+}
